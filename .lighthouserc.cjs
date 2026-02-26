ed362972fc0dad87c8d4a7efbf7d723cb7e219d2
marboqmar <martamartinezboquer@gmail.com>
2026-02-26 13:03:38 +0100

    chore: implement Lighthouse CI with GitHub Actions

---
+++ .lighthouserc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.8 }],
+        'categories:best-practices': ['error', { minScore: 0.8 }],
+        'categories:seo': ['error', { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
